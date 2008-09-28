5c988a438489d5c039474351ef3696274d23440a
LPFaint99 <lpfaint99@gmail.com>
2008-09-28 05:38:43 +0000

    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@36 afb4b194-8be5-11dd-887c-ed04d4280016

--- functions/newTR.php
+++ functions/newTR.php
@@ -1,75 +1,50 @@
 <?php function newTR(&$page,&$accounts){
-	echo "<table width=98% border=3>";
+	echo "<table border=3 align=center>";
 	$new = newestTransaction();
 	if (isset($_POST['XXXxxx'])){
 		$nber=$_POST['dayo'];
 	}
 	else{$nber=2;
 	}
-	//<td colspan=2>
-	echo "<tr><form action=\"" . $_SERVER['PHP_SELF']./*  "?page="
-		. $page . */ "\" method=\"post\"><td>";
-	daydropdown($tt,'o',10);
-	echo "</td><td><input type=\"submit\""
+	
+	echo "<tr><form action=\"" . $_SERVER['PHP_SELF']. "\" method=\"post\"><td colspan=4 align=right>";
+	daydropdown($nber,'o',10);
+	echo "</td><td colspan=3 align=left><input type=\"submit\""
 		. " name=\"XXXxxx\" value=\"";
 	echo "more boxes";
 	echo "\" style=\"background-color: abcdef;\">\n  ";
 	echo "</td></form>";//</tr><tr>
 	
 	
-	echo "\n    <form action=\"" . $_SERVER['PHP_SELF']./*  "?page="
-		. $page . */"\" method=\"post\">";
-		
-/* 	edittrans(true,$page,$accounts,$new,$new);
-	echo "<td align=center><input type=\"submit\""
-		. " name=\"XXX\" value=\"";
-	echo "Add new Transaction";
-	echo "\" style=\"background-color: abcdef;\">\n  ";
-	echo "</td>"; */
-	//echo "</tr>";
+	echo "\n    <form action=\"" . $_SERVER['PHP_SELF']."\" method=\"post\">";
+
 	
 
 	for($tt=0;$tt<$nber;$tt++){
 		echo "</tr>";
 		edittrans(true,$page,$accounts,$new+$tt,$new+$tt);
-		if($tt+1==$nber){	echo "<td align=center><input type=\"submit\""
+	}
+	echo "</tr><tr>";
+	echo "<td align=center colspan=7><input type=\"submit\""
 		. " name=\"XXX\" value=\"";
 	echo "Add new Transaction";
 	echo "\" style=\"background-color: abcdef;\">\n  ";
-	echo "</td>";}
-	}
-	echo "</form>";
+	echo "</td>";
 	
+	echo "</form>";
+	echo "</tr>";
 	
 	
 	
 	if (isset($_POST['XXX'])){
-		// if(submitTransaction($new)){
-			// for($tt=1;$tt<$nber;$tt++){
-				// if(submitTransaction(($new+$tt))){
-				// /* 	$new += 1; */
-					// echo "tt\n$tt\n$new<br><BR><BR><BR>";
-				// }else{		/* echo $tt; */
-				// echo "errortt\n$tt\n$new";
-				// }
-			// }
-			// /* $new += 1;
-			 // */
-			// //reloadPHP();
-		// }
+	
 		for($i=0;$i<=15/* $nber */;$i++){
 			submitTransaction(($new+$i));
 			
 		}
 		reloadPHP();
 	}
-/* 	$X = "X";
-	$X .= $new;
-	if (isset($_POST[$X])){
-		if(submitTransaction($new)){
-			reloadPHP();
-		}
-	}	 */
-	echo "</table>";
+
+	
 }
 ?>
