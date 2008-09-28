40c9b458da8a055d63633ce8b1849703114046ae
LPFaint99 <lpfaint99@gmail.com>
2008-09-28 02:54:39 +0000

    Financial_0.9.2.2.0b
    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@30 afb4b194-8be5-11dd-887c-ed04d4280016

---
+++ new.php
@@ -0,0 +1,33 @@
+ 
+ 
+ 
+ 
+ <?php 
+ echo sumMonth("to",0,1,15,2005) . "<br>";
+ echo sumMonth("from",0,0,0,0);
+function sumMonth($toOrFrom,$account,$month,$day,$year){
+ 
+	if(!$month){$month =(int)date("m");}
+	//echo $month . " month . <br>";
+	$month2 = $month-1;
+	//echo $month2 . " month2 . <br>";
+	if(!$day){$day= (int)date("d");}
+	//echo $day . " day . <br>";
+	if(!$year){$year= (int)date("Y");}
+	//echo $year . " year . <br>";
+	 if($month2){
+		$year2=$year;
+		}
+	else{$month2 = 12; $year2 = $year -1;}
+			//echo $month2 . " month2 . <br>";
+			//echo $year2 . " year2 . <br>";
+
+ 
+	return "SELECT SUM( `amount` ) FROM `transactions` WHERE ((("
+	. "`month` =" . $month . " && `day` > " . $day 
+	. " && `year` =" . $year	. ") OR ("
+	. "`month` =" . $month2 . " && `day` < " . $day
+	. " && `year` =" . $year2 . ")) && ( "
+	. "`" . $toOrFrom . " account` =". $account . " )) LIMIT 0 , 30 ";
+}
+?>
