39a496adb5e47dc0c95e10ec92ec72a944a7ec19
LPFaint99 <lpfaint99@gmail.com>
2009-01-04 05:16:13 +0000

    a few changes
    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@90 afb4b194-8be5-11dd-887c-ed04d4280016

--- index.php
+++ index.php
@@ -2,59 +2,77 @@
 define('TR','\n  <tr>');
 define('TR_','\n  </tr>');
 define('TRo','\n  <tr');
+
 	error_reporting(0);
-	if(!file_exists("f-config.php")){
+	
+	if (!file_exists("f-config.php"))
+	{
 		header('Location: setup/');
 	}
 	require_once("f-config.php");
 	$page = $_GET['page'];
 	$accounttype;
-	$accounts;	$accounts2;	$accounts3;
+	$accounts;
+	$accounts2;
+	$accounts3;
 	$connection = mysql_connect(HOSTNAME, USERNAME, PASSWORD)
-		or die("Unable to connect !\n is your database set up?<a href=\"setup\">setup</a>");
+		or die("Unable to connect !\n is your database set up?"
+				"<a href=\"setup\">setup</a>");
 	mysql_select_db(DATABASENAME)
-		or die('Unable to select database! DATABASENAME\n is your database set up?<a href=\"setup\">setup</a>');
-
+		or die("Unable to select database! DATABASENAME\n is your"
+				"database set up?<a href=\"setup\">setup</a>");
 ?>
 <html>
 <head><title>Financial 0.9.7.0.0</title>
- 	<!--<link href="<?php// echo$app; ?>support/styles.css" rel="stylesheet" type="text/css">-->
-		
-	<link href="resources/financialstyles.css" rel="stylesheet" type="text/css">
+<!--<link href="<?php// echo$app; ?>support/styles.css" rel="stylesheet" type="text/css">-->
+<link href="resources/financialstyles.css" rel="stylesheet" type="text/css">
 </head>
 <body>
 <?php
-	if($page=='setup'){
+	if ($page=='setup' || $page'-1')
+	{
 		$page=-1;
 		$setup=true;
-	}else{
+	}
+	else
+	{
 		setupAcc($accounttype,$accounts,$accounts2,$accounts3);
+	}
+	//________________________________________//
+	if ($page > 0)
+	{
+		pagelayout2($page,$accounttype,$accounts);
+	}
+	else
+	{
+		if (!$setup)
+		{
+			billsDue($accounts2,$page);
+?><!-- <table><tr><TD  width=19% valign=top></td><TD width=60%> --><?
+			mainPage($page,$accounttype,$accounts,$accounts2,$accounts3);
+?><!-- </td><td width=19% valign=top> --><?
+			totals($accounts,$accounts3,$accounttype);
+?><!--</td></tr></table>--><?
+			newTR(0,$accounts);
 		}
-	if($page > 0){pagelayout2($page,$accounttype,$accounts);}
-	else{if(!$setup){?>
-		<?
-		billsDue($accounts2,$page);
-	?>	<!-- <table><tr><TD  width=19% valign=top></td><TD width=60%> -->
-	<?	mainPage($page,$accounttype,$accounts,$accounts2,$accounts3);
-	?>	<!-- </td><td width=19% valign=top> -->
-	<?	totals($accounts,$accounts3,$accounttype);
-	?><!-- 	</td></tr></table>-->
-	<?	newTR(0,$accounts);
-		}
-	if($page){
-		echo "<table border=3 align=center><tr>";
-		$i =editAcc('new',$accounttype);
-		$foo = "account" . $i;
-		if(isset($_POST[$foo])){
-			if(submitAcc($i,'new')){
-				reloadPHP();
+		if ($page)
+		{
+			echo "<table border=3 align=center><tr>";
+			$i =editAcc('new',$accounttype);
+			$foo = "account" . $i;
+			if (isset($_POST[$foo]))
+			{
+				if(submitAcc($i,'new'))
+				{
+					reloadPHP();
+				}
 			}
-		}
-		echo "</tr></table>";
+			echo "</tr></table>";
 		}
 	}
 	mysql_close($connection); 
-?></body>
+?>
+</body>
 </html>
 
 
