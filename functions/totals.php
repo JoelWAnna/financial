4a7fcfff51a46189e9fc40b3e69c4462108cdc5e
LPFaint99 <lpfaint99@gmail.com>
2009-08-24 19:27:17 +0000

    fixes for totals.php
    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@161 afb4b194-8be5-11dd-887c-ed04d4280016

--- functions/totals.php
+++ functions/totals.php
@@ -1,29 +1,35 @@
 <?php function totals(&$accounts,&$accounts3,&$accounttype){?>
 	<div id="Totals"><table border=3>
 <?	for($index=0;$index < 100; $index++){
-	
+	if(!(($accounttype[$index] == "Expense") ||
+		 ($accounttype[$index] == "Income") ||
+		 ($accounttype[$index] == "removed")))
+	{
 	$acc[$accounttype[$index]] =0;
 		for($F=0;$F<20;$F++){
 			if($accounts3[$F]==$accounttype[$index]){
 				$acc[$accounttype[$index]] += currentAmount($F);
-			}	
+			}
 		}
 		if($acc[$accounttype[$index]]){
 			$total += $acc[$accounttype[$index]];
 			
 		?><tr><td align=center><?
 		echo "<div";
-		if($acc[$accounttype[$index]]<0){echo " id=\"negative\"";}else{echo ">";}
+		if($acc[$accounttype[$index]]<0){echo " id=\"negative\"";}
+		echo ">";
 		echo $acc[$accounttype[$index]];
 		echo "</div>";
 		?></td><td align=center><?
 		
 		echo "<div";
-		if($total<0){echo " id=\"negative\"";}else{echo ">";}
+		if($total<0){echo " id=\"negative\"";}
+		echo ">";
 		echo $total;
-		echo "</div>";		
-	   ?></td></tr><?
+		echo "</div>";
+		?></td></tr><?
 		}
+	}
 	}?></table></div>
 <?
 }
