c3e05e228579a14444d46dc8faa635ea53ea9db1
LPFaint99 <lpfaint99@gmail.com>
2009-10-02 19:37:26 +0000

    more boring cleanup
    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@169 afb4b194-8be5-11dd-887c-ed04d4280016

--- functions/newTR.php
+++ functions/newTR.php
@@ -1,49 +1,81 @@
-<?php function newTR($page,&$accounts){
-	?><div id="newtr"><table border=3 align=center><?
-	$nber=$_POST['dayo'];////////////////////////////////
-	if(!$nber){$nber=1;}
+<?php function newTR($page,&$accounts)
+{
+
+	echo  "<div id=\"newtr\">"
+		. "<table border=3 align=center>";
+
+	$nber=$_POST['dayo'];
+
+	if(!$nber)
+	{
+		$nber=1;
+	}
+	
 	$billorTrans = 'trans';
-	if(isset($_POST['bills']) | isset($_POST['billsSubmit']))
-	{	$billorTrans = 'bill';	}
-	addMore($nber,'o');////////////////////////////////
+	if (isset($_POST['bills']) | isset($_POST['billsSubmit']))
+	{
+		$billorTrans = 'bill';
+	}
+	addMore($nber,'o');
 	$num = newest($billorTrans);
 	echo  "\n    <form action=\"" . $_SERVER['PHP_SELF']
 		. "?page=$page&num=$nber\" method=\"post\">";
 
-	
-	for($j=0;$j<$nber;$j++){
+
+	for ($j = 0; $j < $nber; $j++)
+	{
 		editItem($billorTrans,$page,$accounts,($num+$j),true,true);
 		echo "</tr>";
 	}
-	if($billorTrans == 'trans'){
+	
+	if($billorTrans == 'trans')
+	{
 		$billorTrans .= 'action';
 	}
-	echo "<tr><td align=center colspan=7><input type=\"submit\""
-		. " name=\"" . $billorTrans . "sSubmit\" value=\""
-		. "Add new ". $billorTrans . "s\" style=\"background-color:"
-		. " abcdef;\">\n  </td>" . "</form>" . "</tr>";	
-	$submit=$billorTrans.'sSubmit';
-	if (isset($_POST[$submit])){
-		if($billorTrans=='transaction'){$billorTrans='trans';}
+
+	$submit = $billorTrans . 'sSubmit';
+	
+	echo  "<tr>"
+		. "<td align=center colspan=7>"
+		. "<input type=\"submit\" name=\"$submit\" "
+		. "value=\"Add new $billorTrans" . "s\" "
+		. "style=\"background-color: abcdef;\">\n"
+		. "  </td>"
+		. "</form>"
+		. "</tr>";
+
+
+	if (isset($_POST[$submit]))
+	{
+		if($billorTrans=='transaction')
+		{
+			$billorTrans='trans';
+		}
 		$previousSubmit = $_GET['num'];
-		for($i=0;$i<$previousSubmit;$i++){
+		
+		for ($i = 0; $i < $previousSubmit; $i++)
+		{
 			submitItem($billorTrans,($num+$i));
 		}
 		reloadPHP();
 	}
-	?></div><?
-}?>
-<?php function addMore($number,$letter){
-	echo "<tr><form action=\"" . $_SERVER['PHP_SELF']. "\" method=\"post\"><td colspan=7 align=center>";
+	echo "</div>";
+}
+?>
+
+<?php function addMore($number,$letter)
+{
+	echo  "<tr>"
+		. "<form action=\"" . $_SERVER['PHP_SELF']. "\" method=\"post\">"
+		. "<td colspan=7 align=center>";
+
 	dropDown(d,$letter,$number,10);
-	echo "<input type=\"submit\""
-		. " name=\"transactions\" value=\"";
-	echo "more transactions";
-	echo "\" style=\"background-color: abcdef;\">\n  ";
-	echo "<input type=\"submit\""
-		. " name=\"bills\" value=\"";
-	echo "more Bills";
-	echo "\" style=\"background-color: abcdef;\">\n  ";
-	echo "</td>
-	</form></tr>";
-}?>
+
+	echo "<input type=\"submit\" name=\"transactions\" "
+		. "value=\"more transactions\" style=\"background-color: abcdef;\">\n";
+	echo  "  <input type=\"submit\" name=\"bills\" "
+		. "value=\"more Bills\" style=\"background-color: abcdef;\">\n";
+	echo  "  </td>"
+		. "</form></tr>";
+}
+?>
