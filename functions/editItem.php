f028795545c3dd32ae2779cb8db51b0d0730d88e
LPFaint99 <lpfaint99@gmail.com>
2009-10-04 04:21:19 +0000

    attempt to show proper available dates + cleanup (hard to test without a local server :p)
    git-svn-id: http://lpfaint99-svn.googlecode.com/svn/trunk@174 afb4b194-8be5-11dd-887c-ed04d4280016

--- functions/editItem.php
+++ functions/editItem.php
@@ -6,17 +6,33 @@
 	{
 		return;
 	}
-
 						
-		if (($type == 'transaction') && !$noforms)
-		{
-			echo "\n    <tr><form action=\"" . $_SERVER['PHP_SELF']
-				. "?page=" . $page . "\" method=\"post\">";
-		}
+	if (($type == 'transaction') && !$noforms)
+	{
+		echo "\n    <tr><form action=\"" . $_SERVER['PHP_SELF']
+			. "?page=$page\" method=\"post\">";
+	}
 	echo "<td width=55>";
 	echo dropDownDate(m, $number, $month, 12);
 	echo "</td><td width=50>";
-	echo dropDownDate(d, $number, $day, 31);
+	
+	$maxDay = 31;
+	switch ($month)
+	{
+	case 2:
+		$maxDay--;
+		if (!($year%100) || ($year%4))
+		{	// Not Leap Year
+			$maxDay--;
+		}
+	case 4: case 6: case 9: case 11:
+		$maxDay--;
+		break;
+	default:
+		break;
+	}
+	
+	echo dropDownDate(d, $number, $day, $maxDay);
 	echo "</td><td width=55>";
 	echo dropDownDate('Y',$number,$year, 2);
 	echo "</td><td width=142>";
@@ -31,17 +47,18 @@
 	echo "</td><td width=50>";
 	textField($number, $amount, 'amount');
 	echo "</td>";
-		if (($type == 'transaction') && !$noforms)
-		{
-			echo "<td colSpan=\"2\" align=center>";
-			echo"<input type=\"submit\" name=\"X"
-				.$number. "\" value=\"";
-			if($newtrans){
-				echo "Add New Transaction ";
-			}else{
-				echo "Submit Changes to ";
-			}	
-			echo  $number . "\" style=\"background-color: "
-				. "abcdef;\"></td>\n  </form></tr>";
-		}
+
+	if (($type == 'transaction') && !$noforms)
+	{
+		echo "<td colSpan=\"2\" align=center>";
+		echo"<input type=\"submit\" name=\"X"
+			.$number. "\" value=\"";
+		if($newtrans){
+			echo "Add New Transaction ";
+		}else{
+			echo "Submit Changes to ";
+		}	
+		echo  $number . "\" style=\"background-color: "
+			. "abcdef;\"></td>\n  </form></tr>";
+	}
 }?>
