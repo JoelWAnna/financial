5a38fff1d23e7e43d8a3d80709191148ec3eb642
LPFaint99 <lpfaint99@gmail.com>
2013-06-28 21:10:25 -0700

    add some lan fixes

--- functions/Authentication.php
+++ functions/Authentication.php
@@ -1,7 +1,17 @@
 <?php function Authentication(&$connection)
 {
+	try {
+	$connection = new PDO("mysql:host=" . HOSTNAME . ";port=3306;dbname=" . DATABASENAME . ";charset=UTF8", USERNAME, PASSWORD );
+	$connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+	$connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+	}
+catch(PDOException $e) {
+    echo "Error!: " . $e->getMessage() . "<br/>\n";
+	 die("Unable to connect !\n is your database set up?".
+				"<a href=\"setup\">setup</a>");
+}
 	extract($_SERVER);
-	if ($REMOTE_ADDR == "127.0.0.1" || $REMOTE_ADDR =="::1") return "Admin++";
+	if ($REMOTE_ADDR == "127.0.0.1" || $REMOTE_ADDR =="::1" || $REMOTE_ADDR=="192.168.1.100") return "Admin++";
 	$ret = "invalid";
 	if(isset($_POST['login']) && isset($_POST['pwd']))
 	{
@@ -10,34 +20,33 @@
 	}
 	else
 	{
-		if (!isset($_SESSION['login'])  || !isset($_SESSION['pwd']))
+		if (!isset($_SESSION['login']) || !isset($_SESSION['pwd']))
 		{
 			loginForm();
 			return $ret;
 		}
 	}
 	$login_query = " Select `privileges` from " . PREFIX. "users" .
-					" Where `login` =:login" .
-					" AND `pwd` =:pwd;";
+					" Where `login` = :login" .
+					" AND `pwd` = :pwd;";
 	$stmt = $connection->prepare($login_query);
-	$stmt->bindParam(":login", $_SESSION['login']);
-	$stmt->bindParam(":pwd", $_SESSION['pwd']);
-
-
-	if (!$stmt->execute())
+	$stmt->bindParam(":login", $_SESSION['login'], PDO::PARAM_STR);
+	$stmt->bindParam(":pwd", $_SESSION['pwd'], PDO::PARAM_STR);
+	$login_result = $stmt->query()
+	or die ("Error in query: line $login_result" . mysql_error());
+	if ($login_result->rowCount() != 1)
 	{
 		echo "login failed";
 			loginForm();
 			return $ret;
 	}
-	$rowdata = $stmt->fetch();
+	$rowdata = $login_result->fetch();
 
 	switch($rowdata['privileges'])
 	{
 	case 0:
 		$ret = "guest";
 		break;
-
 	case 1:
 		$ret = "admin";
 		break;
@@ -49,25 +58,21 @@
 	}
 	return $ret;
 }
-
-function loginForm()
-{
-		echo "\n    <form action=\"" . $_SERVER['PHP_SELF']
+function loginForm() {
+		echo "\n <form action=\"" . $_SERVER['PHP_SELF']
 				. "?page=$page\" method=\"post\">\n"
-				. "username\t"//. "    <li class=\"\">"
+				. "username\t"//. " <li class=\"\">"
 				. textField("", "", "login")
 				//. "</li>\n"
-				. "<br>\npassword\t"//. "    <li class=\"\">"
+				. "<br>\npassword\t"//. " <li class=\"\">"
 				. textField("", "", "pwd", "password")
 				;//. "</li>\n";
 			echo "<input type=\"submit\" name=\"loginsubmit\" value=\"";
 			echo "Login\" style=\"background-color: "
-			. "abcdef;\">\n  </form>";
+			. "abcdef;\">\n </form>";
 }
 ?>
-
-<?php function userIsAdmin()
-{
-	return ($GLOBALS['authentication'] != "guest" && $GLOBALS['authentication'] != "invalid");
+<?php function userIsAdmin() {
+	return ($GLOBALS['authentication'] != "guest" && $GLOBALS['authentication'] !="invalid");
 }
 ?>
